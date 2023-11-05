36e452c282240f2807b8f06fb9106fb3c4e1df72
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2023-11-05 22:09:25 +0200

    rename branch

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -17,14 +17,6 @@ const addContact = async (req, res, next) => {
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
-const deleteContact = async (req, res, next) => {
-  const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
-  if (!result) {
-    throw HttpError(404, "Not found");
-  }
-  res.status(200).json({ message: "contact deleted" });
-};
 const updateContact = async (req, res, next) => {
   const { contactId } = req.params;
   const result = await contacts.updateContact(contactId, req.body);
@@ -33,6 +25,15 @@ const updateContact = async (req, res, next) => {
   }
   res.status(200).json(result);
 };
+const deleteContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({ message: "contact deleted" });
+};
+
 module.exports = {
   getAll: ctrlWrapper(getAll),
   getById: ctrlWrapper(getById),
